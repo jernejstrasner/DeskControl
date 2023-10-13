6ed7b375f79bad2c07d37bd624297b8599bc20f4
jernejstrasner <jernejstrasner@users.noreply.github.com>
2023-10-13 09:14:48 +0200

    Basic interface done

--- DeskControliOS/ContentView.swift
+++ DeskControliOS/ContentView.swift
@@ -10,11 +10,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .center) {
+            Text("<desk selection>")
+            Text("<desk status>")
+            Button {
+                // TODO
+            } label: {
+                Image(systemName: "chevron.up")
+                    .imageScale(.large)
+            }.buttonStyle(.borderedProminent)
+            Text("99.9")
+                .font(.system(size: 64))
+            Button {
+                
+            } label: {
+                Image(systemName: "chevron.down")
+                    .imageScale(.large)
+            }.buttonStyle(.borderedProminent)
+            Divider()
+            HStack(alignment: .top) {
+                VStack(alignment: .center) {
+                    Text("Sitting position")
+                    Text("99.9")
+                        .font(.system(size: 36))
+                    Button {
+                        
+                    } label: {
+                        Text("Move to")
+                    }
+                    .buttonStyle(.bordered)
+                    Button {
+                        
+                    } label: {
+                        Text("Save")
+                    }
+                    .buttonStyle(.bordered)
+                }.frame(maxWidth: .infinity)
+                Divider()
+                VStack(alignment: .center) {
+                    Text("Standing position")
+                    Text("99.9")
+                        .font(.system(size: 36))
+                    Button {
+                        
+                    } label: {
+                        Text("Move to")
+                    }
+                    .buttonStyle(.bordered)
+                    Button {
+                        
+                    } label: {
+                        Text("Save")
+                    }
+                    .buttonStyle(.bordered)
+                }.frame(maxWidth: .infinity)
+            }.frame(maxWidth: .infinity)
+            Button {
+                
+            } label: {
+                Text("Stop").fontWeight(.bold)
+            }.buttonStyle(.borderedProminent)
         }
         .padding()
     }

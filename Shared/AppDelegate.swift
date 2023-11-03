023907b969d1673d0ff62060899f5ef796bbc822
jernejstrasner <jernejstrasner@users.noreply.github.com>
2023-11-03 20:56:46 +0100

    Remove Sentry debug logs

--- Shared/AppDelegate.swift
+++ Shared/AppDelegate.swift
@@ -14,9 +14,6 @@ struct DeskControlApp: App {
     init() {
         SentrySDK.start { options in
             options.dsn = "https://673adb93f8a210dfc359aac8867c7960@o1146455.ingest.sentry.io/4506157598375936"
-            #if DEBUG
-            options.debug = true // Enabled debug when first installing is always helpful
-            #endif
             
             // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
             // We recommend adjusting this value in production.

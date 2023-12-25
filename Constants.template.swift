9a927bb913aee5fca1139be40681d6bcbe3da0a6
jernejstrasner <jernejstrasner@users.noreply.github.com>
2023-12-25 17:57:30 +0100

    Inject secrets at build time

---
+++ Constants.template.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  DeskControl
+//
+//  Created by Jernej Strasner on 24. 12. 23.
+//
+
+public struct Constants {
+    static let sentryDSN = "${SENTRY_DSN}"
+}
--- Shared/AppDelegate.swift
+++ Shared/AppDelegate.swift
@@ -13,7 +13,7 @@ import Sentry
 struct DeskControlApp: App {
     init() {
         SentrySDK.start { options in
-            options.dsn = "https://673adb93f8a210dfc359aac8867c7960@o1146455.ingest.sentry.io/4506157598375936"
+            options.dsn = Constants.sentryDSN
             
             // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
             // We recommend adjusting this value in production.
